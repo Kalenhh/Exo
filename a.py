1d9245d480660e57ba41ae7a29749c11cac25539
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2021-09-23 14:50:17 +0200

    fin version 1

--- a.py
+++ a.py
@@ -1,19 +1,19 @@
 #coding:utf-8
 
-#déclaration de variable ------------------------------------------------------------------------------------------------
+#déclaration de variable --------------------------------------------------------------------------------------------------------------------
 
 input_principal = str
 
-#déclaration de fonction ---------------------------------------------------------------------------------------------
+#déclaration de fonction -------------------------------------------------------------------------------------------------------------------------
 
-""" 'convertir_ms_kmh(float/int)' ,Convertis une variable de m/s à km/h
+""" 'convertir_ms_kmh(float)' ,Convertis une variable de m/s à km/h
 Une valeur d'entrée , une de sortie"""
 
 def convertir_ms_kmh(v1) :
 	v = v1 * 3.6
 	return v
 
-""" 'convertir_kmh_ms(float/int)' ,Convertis une variable de km/h à m/s 
+""" 'convertir_kmh_ms(float)' ,Convertis une variable de km/h à m/s 
 Une valeur d'entrée , une de sortie"""
 
 def convertir_kmh_ms(v1) :
@@ -28,28 +28,19 @@ def comparaison(Comparé,Comparant) :
 	else :
 		return False
 
-"""
+""" 'try_float(Valeur)' ,Si Valeur peut être converti en float retourne Vrai , sinon retourne Faux"""
+
 def try_float(x) :
 	try :
-		y = float(x)
-		pass
+		x = float(x)
+		return True
 	except :
-		return "erreur d input"
-
-		WIP--------------"""
-
-
-
-
-
-
-
-
+		return False	
 
 """ 'information_conversion_kmh_ms()' ,Donne des informations sur 'conversion_kmh_ms()' """
 
 def information_conversion_kmh_ms() :
-	print("1. de km/h vers m/s\n2. de m/s vers km/h\n")
+	print("1. De km/h vers m/s \n2. De m/s vers km/h \nmenu. Afficher le menu \nexit. Quitter le programme \n")
 
 """ 'conversion_kmh_ms(str)' ,Selon la variable d'entrée , propose un menu à plusieurs sortie possible"""
 
@@ -59,20 +50,31 @@ def conversion_kmh_ms(Choice) :
 				
 		#convertir Vitesse en m/s
 
-		Vitesse = float(input("Valeur en km/h : "))
-		print(Vitesse,"km/h équivaut à ",round(convertir_kmh_ms(Vitesse),3),"m/s.\n")
-
+		Vitesse = input("Valeur en km/h : ")
+		if try_float(Vitesse) == True :
+			print(Vitesse,"km/h équivaut à ",round(convertir_kmh_ms(float(Vitesse)),3),"m/s.\n")
+			return
+		else :
+			print("Rentrez une valeur numérique:\n")
+			return
+	
 	if Choice == "2" :
 				
 		#convertir Vitesse en km/h
 				
 		Vitesse = input("Valeur en m/s :")
-		print(try_float(Vitesse) or Vitesse,"m/s équivaut à ",round(convertir_ms_kmh(float(Vitesse)),3),"km/h.\n") #WIP--------------
-			
-	elif Choice == "menu" :
+		if try_float(Vitesse) == True :
+			print(Vitesse,"m/s équivaut à ",round(convertir_ms_kmh(float(Vitesse)),3),"km/h.\n")
+			return
+		else :
+			print("Rentrez une valeur numérique:\n")
+			return
+
+	elif Choice.lower() == "menu" :
 		information_conversion_kmh_ms()
 
-	elif Choice == "exit" :
+	elif Choice.lower() == "exit" :
+		print("-"*150)
 		pass
 
 	else :
