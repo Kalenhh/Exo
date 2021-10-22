8f3fe8b73ee689e26688a6ce44f756c0447b14b8
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2021-10-22 15:53:43 +0200

    sa

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
 #coding:utf-8
 
-print(1.2+1.27)
+tried = ""
+liste_number = open("C:/Users/Elève/Desktop/number.txt","r")
+contenu = liste_number.readlines()
+print(contenu)
+
+cont = []
+for un in contenu :
+	un = un.strip()
+	cont.append(un)
+
+print(cont)
+print(len(cont))
+
+for element in cont :
+	contsanselement = cont
+	print("test de",element)
+	del(contsanselement[contsanselement.index(element)])
+	for i in contsanselement :
+		if element == i :
+			print("doublon")
+		else :
+			continue
+
+print(cont)
+print(len(cont))
