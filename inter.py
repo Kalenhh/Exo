85ad9bbce7a243db4c21921099bcad16ddce82f9
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2021-11-02 17:42:35 +0100

    corps du logi

--- inter.py
+++ inter.py
@@ -2,20 +2,16 @@
 
 from tkinter import*
 from tkinter.filedialog import *
-
+import random
 
 app = Tk()
 app.title("Sauce Folder")
-app.minsize(300,200)
+app.minsize(600,400)
 
 main_frame = Frame(app,bg="yellow")
-main_frame.pack(side="right",fill=BOTH,expand=1)
+main_frame.pack(fill=BOTH,expand=1)
 
-texte = Label(main_frame, text = "application")
-texte.pack(side="top")
-texte.configure(fg = "blue")
-
-#----------------------------------------------------------------------------------------------------------
+#Fonction data :----------------------------------------------------------------------------------------------------------
 
 def comparer_doublon() :
 	with open("C:/Users/baudo/Desktop/number.txt","r") as liste_number :
@@ -44,16 +40,61 @@ def quantity() :
 		liste_number_lignes = liste_number.readlines()
 		quantite.set(len(liste_number_lignes))
 
-#-------------------------------------------------------------------------------------------------------------
+def random() :
+	with open("C:/Users/baudo/Desktop/number.txt","r") as liste_number :
+		liste_number_lignes = liste_number.readlines()
+		random.set(liste_number_lignes[random.randrange(len(liste_number_lignes))])
 
-FrameMenu = Frame(app,bg="blue",borderwidth=10)
-FrameMenu.pack(side="left",fill=BOTH)
+#Fonction Tk.Frame :-------------------------------------------------------------------------------------------------------------
 
-comparer = Button(FrameMenu,text="Comparer")
-comparer.pack()
+def menu():
+	FrameMenu = Frame(main_frame,bg="blue",borderwidth=10)
+	FrameMenu.pack(side="left",fill=BOTH)
 
-nettoyer = Button(FrameMenu,text="clear")
-nettoyer.pack(fill=BOTH)
+	comparer = Button(FrameMenu,text="Comparer",command=frame_comparer)
+	comparer.pack()
+
+	doublon = Button(FrameMenu,text="clear",command=frame_doublon)
+	doublon.pack(fill=BOTH)
+
+	stat = Button(FrameMenu,text="stat",command=frame_stat).pack(fill=BOTH)
+
+def frame_stat():
+	global frame_affichage
+	frame_affichage.destroy()
+	frame_affichage = Frame(main_frame)
+	
+	frame_affichage.pack(side="right",fill=BOTH,expand=1)
+	stat = Label(frame_affichage,text="ok stat").pack()
+
+
+def frame_comparer():
+	global frame_affichage
+	frame_affichage.destroy()
+	frame_affichage = Frame(main_frame)
+	frame_affichage.pack(side="right",fill=BOTH,expand=1)
+
+	FrameComparer = Frame(frame_affichage,bg="green")
+	FrameComparer.pack(side="right",fill=BOTH,expand=1)
+
+
+def frame_doublon():
+	global frame_affichage
+	frame_affichage.destroy()
+	frame_affichage = Frame(main_frame)
+	frame_affichage.pack(side="right",fill=BOTH,expand=1)
+
+	FrameDoublon = Frame(frame_affichage,bg="pink")
+	FrameDoublon.pack(side="right",fill=BOTH,expand=1)
+
+
+#Programme :---------------------------------------------------------------------------------------------------------
+
+menu()
+frame_affichage = Frame(main_frame)
+frame_affichage.pack(side="right",fill=BOTH,expand=1)
+frame_stat()
+frame_doublon()
 
 
 
