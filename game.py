9cc8b0f5674c3a51c168070bc2c49c015dd9eca3
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2021-11-19 15:46:05 +0100

    V2

--- game.py
+++ game.py
@@ -9,28 +9,44 @@ valeur = ""
 nombre = 0
 n = 0
 Lock = True
+t1 = 0
+t2 = 0
+ResponseTime = 0
+Score = 0
+Player = ""
+
 
 # Prog :____________________________________________________________________________________________________________________
 
 print("Jeu du +/-\n")
 while Lock == True :
-	valeur = input("Faire une partie ? y/n :")
+	valeur = input("\nFaire une partie ? y/n/h :")
 
 	if valeur == "n" :
 		Lock = False
 		continue
 
-	if valeur == "y" :
+	if valeur == "y" :    #------------------------------------------------------------------------  Jeu \/
 		
 		n = 0
 		nombre = randint(1,100)
 		while n != 15 :
+
+			t1 = time()
 			valeur = input("Prosition de valeur : ")
 			n = n+1
+			t2 = time()
+
+			ResponseTime = ResponseTime + t2-t1
+
 
 			valeur = int(valeur)
 			if valeur == nombre :
-				print("Gagné en ",n," tours !\n")
+				Score = round(ResponseTime * n,1)
+				print("Gagné en ",n," tours !\nVotre score est de ",Score)
+				Player = input("Enregistrez le score sous le nom : ")
+				with open("game_log.txt","a") as file :
+					file.write(Player+";"+str(Score))
 				n = 15
 
 			if valeur <  nombre :
@@ -40,7 +56,21 @@ while Lock == True :
 				print("Trop grand")
 
 		if valeur != nombre :
-			print("Nombre de tours dépassé !\nLa valeur recherchée était ",nombre?"\n")
+			print("Nombre de tours dépassé !\nLa valeur recherchée était ",nombre,"\n")  #-------------------------------
 
+	if valeur == "h" :
+		#Afficher les best scores		
+		with open("game_log.txt","r") as file :
+			best = 100000000000000000000
+			file = file.read()
+			file = file.split("\n")
+			while 10 :
+				for i in file :
+					i = i.split(";")
+					if float(i[1]) < best :
+						best = i
+						file.remove(i)
+				print(best)
+			
 	else :
 		print("Entrez 'y' ou 'n'")		
