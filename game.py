368d3733f113ee6aca9589ccc8c31ac9f67a2f44
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2021-11-19 14:07:53 +0100

    V1

--- game.py
+++ game.py
@@ -1,11 +1,46 @@
 #coding:utf-8
 
-chaine = input("rentrez une chaine :")
-chaine_fini = ""
-for i in chaine :
-	if i == "e" :
-		chaine_fini= chaine_fini + "A"
-		continue
-	chaine_fini = chaine_fini + i
+from random import*
+from time import*
 
-print(chaine_fini)
+# Variables :______________________________________________________________________________________________________________
+
+valeur = ""
+nombre = 0
+n = 0
+Lock = True
+
+# Prog :____________________________________________________________________________________________________________________
+
+print("Jeu du +/-\n")
+while Lock == True :
+	valeur = input("Faire une partie ? y/n :")
+
+	if valeur == "n" :
+		Lock = False
+		continue
+
+	if valeur == "y" :
+		
+		n = 0
+		nombre = randint(1,100)
+		while n != 15 :
+			valeur = input("Prosition de valeur : ")
+			n = n+1
+
+			valeur = int(valeur)
+			if valeur == nombre :
+				print("Gagné en ",n," tours !\n")
+				n = 15
+
+			if valeur <  nombre :
+				print("Trop petit")
+
+			if valeur > nombre :
+				print("Trop grand")
+
+		if valeur != nombre :
+			print("Nombre de tours dépassé !\nLa valeur recherchée était ",nombre?"\n")
+
+	else :
+		print("Entrez 'y' ou 'n'")		
