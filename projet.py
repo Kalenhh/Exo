6db51bdbcf8c653c02f795ca4ec3f0d19aa5696d
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2021-12-06 18:49:44 +0100

    g

--- projet.py
+++ projet.py
@@ -3,17 +3,43 @@
 #Projet : Lecture et traitement de fichier CSV 
 
 from csv import*
+from tkinter import*
+from time import*
+
+root = Tk()
+root.minsize(600,400)
+frame = Frame(root)
+frame.pack()
+
+sss = StringVar()
+
 
 def ouverture(fichier) :
+	global progress
+	global bar
+
 	with open(fichier,"r",encoding="utf-8") as donné :
 		aaa = reader(donné,delimiter=";")
-		table = []
+		table = ["1","A","2020","0"]
+		letter = "A"
+		progress = 0 
 		for i in aaa :
+			if i[1][0] != letter:
+				letter = i[1][0]
+				progress = progress + 1
+				bar = progress *100/434
+				print(int(round(bar,0)))
+				sss.set(bar)
+
+
 			table.append(i)
+
 		return table
 
 vued = ouverture("nat2020.csv")
-print(vued)
+print(len(vued))
+
+
 
 def requete1(donné,prenom,année) :
 	trouvé = False
@@ -24,3 +50,7 @@ def requete1(donné,prenom,année) :
 				return i[3]
 				continue
 
+lab = Label(frame,textvariable=sss).pack()
+
+
+root.mainloop()
