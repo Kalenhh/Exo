8bb7d4749929249e152946f52c2fd2a420536c3c
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2022-01-07 15:30:36 +0100

    sdfhgh

--- projet.py
+++ projet.py
@@ -4,20 +4,18 @@
 
 from csv import*
 
+def ouverture(root) :
 
-def ouverture(fichier) :
-
-	with open(fichier,"r",encoding="utf-8") as donné :
-		aaa = reader(donné,delimiter=";")
+	with open(root,"r",encoding="utf-8") as fichier :
+		lecteur = reader(fichier,delimiter=";")
 		table = []
-		for i in aaa :
+		for i in lecteur :
 			table.append(i)
 
 		return table
 
-vued = ouverture("nat2020.csv")
-print(len(vued),"c good")
-
+source = ouverture("nat2020.csv")
+print("La liste est de",len(source),"ligne.\n")
 
 
 def requete1(donné,prenom,année) :
@@ -37,23 +35,75 @@ def requete1(donné,prenom,année) :
 
 		i = i + 1
 
+
+
 def requete2(donné,année) :
 	
-	donnéG = [i for i in donné if i[0]=="1" if i[2]==année]
-	donnéF = [i for i in donné if i[0]=="2" if i[2]==année]
-	
-	garcon =
+	donnéG = [i for i in donné if i[0]=="1" if i[2]==année if i[1] != "_PRENOMS_RARES"]
+	donnéF = [i for i in donné if i[0]=="2" if i[2]==année if i[1] != "_PRENOMS_RARES"]
+
+	garcon = ()
+	fille = ()
+
+	best = 0 
+	for i in donnéG :
+		if int(i[3]) > best :
+			best = int(i[3])
+			garcon = (i[1],best)
+
+	best = 0
+	for i in donnéF :
+		if int(i[3]) > best :
+			best = int(i[3])
+			fille = (i[1],best)
+
+	return garcon , fille
 
 
 
-	for i in donné :
-		if i == 1
-			if i2 == année
-				if i > best :
-					best = i
+def requete3(donne,prenom) :
 
-requete2(vued,"2005")
+	maxi = ()
+	best = 0
+	for i in donne :
+		if i[1] == prenom and int(i[3]) > best :
+			best = int(i[3])
+			maxi = (i[2],best)
+	if maxi == () :
+		return None
+	else :
+		return maxi	
+				
 
 
-	
 
+Lock = True
+
+while Lock == True :
+
+	print("pavé césar")
+
+	choix = input("> ")
+
+	if choix == "q" :
+		Lock = False
+		continue
+
+	if choix == "1" :
+		pre = input("Prénom :")
+		ann = input("Année :")
+		print(requete1(source,pre.upper(),ann))
+		continue
+
+	if choix == "2" :
+		anné = input("Année :")
+		print(requete2(source,anné))
+		continue
+
+	if choix == "3" :
+		pren = input("Prénom :")
+		print(requete3(source,pren.upper()))
+		continue
+
+	else :
+		print("Erreur")
