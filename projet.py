295991e65473914e5cd2da529345f035eb905a81
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2022-01-11 13:11:40 +0100

    j

--- projet.py
+++ projet.py
@@ -2,7 +2,11 @@
 
 #Projet : Lecture et traitement de fichier CSV 
 
+# Import :--------------------------------------------------------------------------------------------------------
+
 from csv import*
+
+# Fonctions :---------------------------------------------------------------------------------------------------
 
 def ouverture(root) :
 
@@ -14,27 +18,29 @@ def ouverture(root) :
 
 		return table
 
-source = ouverture("nat2020.csv")
-print("La liste est de",len(source),"ligne.\n")
 
-
-def requete1(donné,prenom,année) :
+def requete1(donné,prenom,genre,année="XXXX") :
 	trouvé = False
 	i=0
+
+	if genre == "m" :
+		genre = "1"
+	if genre == "f" :
+		genre = "2"
+
 	while trouvé == False :
 
 		if i == len(donné) :
-			trouvé = True 
-			return None
+			trouvé = True
+			return
 			continue
 
-		if prenom == donné[i][1] and année == donné[i][2] :
+		if prenom == donné[i][1] and année == donné[i][2] and genre == donné[i][0] :
 			trouvé = True
 			return donné[i][3]
 			continue
 
 		i = i + 1
-
 
 
 def requete2(donné,année) :
@@ -45,7 +51,7 @@ def requete2(donné,année) :
 	garcon = ()
 	fille = ()
 
-	best = 0 
+	best = 0
 	for i in donnéG :
 		if int(i[3]) > best :
 			best = int(i[3])
@@ -60,7 +66,6 @@ def requete2(donné,année) :
 	return garcon , fille
 
 
-
 def requete3(donne,prenom) :
 
 	maxi = ()
@@ -68,32 +73,56 @@ def requete3(donne,prenom) :
 	for i in donne :
 		if i[1] == prenom and int(i[3]) > best :
 			best = int(i[3])
-			maxi = (i[2],best)
+			maxi = (i[2],best,i[0])
 	if maxi == () :
 		return None
 	else :
 		return maxi	
 				
+def pavé() :
+	print("╔═════════════════════════════════════════════════════════════════════════════════╗")
+	print("║                        ▲♠▲            MENU DES TACHES.           ▲♠▲            ║")
+	print("║                        ▲♠▲        Que voulez-vous faire ?        ▲♠▲            ║")
+	print("╠═════════════════════════════════════════════════════════════════════════════════╣")
+	print("║1 ► Nombre d'individus qui portent un prénom donné pour une année donnée. ˃      ║")
+	print("║2 ► Prénoms les plus courant pour une année donnée. ˃                            ║")
+	print("║3 ► Année qui correspond au maximum d'un prénom donné. ˃                         ║")
+	print("║4 ► Afficher les options. ˃                                                      ║")
+	print("║5 ► Quitter le programme. ˃                                                      ║")
+	print("╚═════════════════════════════════════════════════════════════════════════════════╝\n")
 
+# Programme :---------------------------------------------------------------------------------------
 
+print("Hello world")
+
+source = ouverture("nat2020.csv")
+print("Fichier chargé :",len(source),"ligne.\n")
+pavé()
 
 Lock = True
-
 while Lock == True :
-
-	print("pavé césar")
 
 	choix = input("> ")
 
-	if choix == "q" :
+	if choix == "5" :
 		Lock = False
 		continue
+
+	if choix == "4" :
+		pavé()
+		continue	
 
 	if choix == "1" :
 		pre = input("Prénom :")
 		ann = input("Année :")
-		print(requete1(source,pre.upper(),ann))
-		continue
+		sexe = input("Prénom masculin ou féminin ,m/f :")
+
+		if ann.isnumeric() == False :
+			print(requete1(source,pre.upper(),sexe.lower),"sur toute la période.")
+			continue
+		else :
+			print(requete1(source,pre.upper(),sexe.lower(),ann),"personnes ont été nommées",pre,"en",ann)
+			continue
 
 	if choix == "2" :
 		anné = input("Année :")
