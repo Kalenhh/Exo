f946575fd1932e2b6067d7599414caa5f7102cc0
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2022-05-05 22:08:55 +0200

    ca marche

--- vide.py
+++ vide.py
@@ -14,7 +14,7 @@ def tri_insert() :
 	"""
 	On prend chaque item et on l'insere juste apres la valeur plus petite que lui
 	"""
-	liste = [o for o in range(100)]
+	liste = [o for o in range(20)]
 	shuffle(liste)
 
 	for i in range(len(liste)) : 	# i est l'index de l'item selectionné
@@ -27,23 +27,23 @@ def tri_insert() :
 		liste.pop(i+1)				# On supprime la valeur i donc i+1
 
 		print(deepcopy(liste))
-		actu(deepcopy(liste))
+		actu(deepcopy(liste),i+1)
 		print(i)
-		pass
 
-def actu(liste) :
-	sleep(1)
+def actu(liste,val) :
+	sleep(2)
 	global can
-	can.destroy()
-	can = Canvas(root,bg="yellow",width=300, height=200)
-	can.pack()
+	can.delete(ALL)
 
 	pla = 10
 	for i in liste :
-		can.create_line(pla,0,pla,i*10,fill="red",width=5,tags="ligne")
+		if liste[i] != val :
+			can.create_line(pla,0,pla,i*10,fill="blue",width=5,tags="ligne")
+		else :	
+			can.create_line(pla,0,pla,i*10,fill="red",width=5,tags="ligne")
 		pla = pla+10
 
-	return	
+	root.update()	
 
 can = Canvas(root,bg="yellow",width=300, height=200)
 can.pack()
