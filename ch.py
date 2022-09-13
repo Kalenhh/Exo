581df7cf3cdcf9e7ab1be7cef32492358624e3b6
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2022-09-13 19:54:15 +0200

    up du jour

--- ch.py
+++ ch.py
@@ -1,26 +1,25 @@
 #coding:utf-8
 # Liste chainée
 
+A = [14,[12,[9,None]]]
+B = [None,None]
+C = [3,None]
+print(f"Liste :\t{A}\t{B}\t{C}")
+
+
 def longueur(chain) :
 
 	if chain == [] :
 		return "Empty List E0"
 
 	longueur = 0
-	while chain != None :
+	while chain != None and chain[0] is not None :
 		longueur += 1
 		chain = chain[1]
 
 	return longueur
-	
 
-A = [14,[12,[9,None]]]
-B = []
-C = [3,None]
-print(A,B,C)
-
-"""
-print("test longueur : ",longueur(A),longueur(B),longueur(C))	"""
+print(f"len\t{longueur(A)}\t{longueur(B)}\t{longueur(C)}")
 
 
 def acceder(chain,i) :
@@ -28,72 +27,130 @@ def acceder(chain,i) :
 	if chain == [] :
 		return "Empty List E1"
 
-	if i > longueur(chain):
+	if i >= longueur(chain):
 		return "Index I1"	
 
-	for o in range(i-1) :
+	for o in range(i) :
 		chain = chain[1]
 
 	return chain[0]
-"""
-print("test acceder : ",acceder(A,1),acceder(B,1),acceder(C,1))
-print("test acceder : ",acceder(A,2),acceder(B,2),acceder(C,2))
-print("test acceder : ",acceder(A,3),acceder(B,3),acceder(C,3))"""
+
+print(f"access\t{acceder(A,0)}\t{acceder(B,0)}\t{acceder(C,0)}")
+print(f"access\t{acceder(A,1)}\t{acceder(B,1)}\t{acceder(C,1)}")
+print(f"access\t{acceder(A,2)}\t{acceder(B,2)}\t{acceder(C,2)}")
+
+print(f"Liste :\t{A}\t{B}\t{C}")
+
 
 def ajouter(chain,element) :
 
 	if len(chain) == 0 :
-		chain = [element,None]
-		return chain
+		print("Empty list E2")
+		return
 
 	seg = chain
 	while seg[1] != None :
 		seg = seg[1]
 
-	seg[1] = [element,None]
-	return chain
+	seg[:] = [seg[0],[element,None]]
 
+	if chain[0] is None :
+		seg[:] = [element,None]
 
+print("\nFonction ajouter :")
+ajouter(A,"ajouter")
+ajouter(B,"ajouter")
+ajouter(C,"ajouter")
+ajouter(A,"ajouter1")
+ajouter(B,"ajouter1")
+ajouter(C,"ajouter1")
 
-print("test ajouter : ",ajouter(A,5),ajouter(B,5),ajouter(C,5))
+print(f"Liste :\n{A}\n{B}\n{C}")
+
 
 def inserer(chain,i,element) :
 
-	if longueur(chain) < i :
-		return "Index big"
-
-	if i == 0 :
-		chain = [element,chain]
-		return chain
+	if longueur(chain) <= i :
+		return "Index I3"
 
 	seg = chain
 	for o in range(i) :
 		seg=seg[1]
 
-	if seg is None :
-		chain = ajouter(chain,element)
-		return chain
+	seg[:] = [element,[seg[0],seg[1]]]
 
-	seg[1] = [seg[0],seg[1]]
-	seg[0] = element
-	
-	return chain
+print("\nFonction inserer :")
+inserer(A,0,"inser0")
+inserer(B,0,"inser0")
+inserer(C,0,"inser0")
+inserer(A,2,"inser2")
+inserer(B,2,"inser2")
+inserer(C,2,"inser2")
+inserer(A,4,"inser4")
+inserer(B,4,"inser4")
+inserer(C,4,"inser4")
 
-print("test inserer : ",inserer(A,0,9))
+print(f"Liste :\n{A}\n{B}\n{C}")
+
 
 def supprimer_val(chain,element) :
 
-	if len(chain) == 0 :
-		print("Vide")
+	if chain == [] :
+		print("Empty List E4")
 
 	seg = chain
 	while seg[0]!=element :
 		seg = seg[1]
 
+	if seg[1] is None :
+		seg[:] = (None,)
+		seg[:] =
+		return
+	seg[:] = seg[1]
+
+
+print("\nFonction supprimer_val :")
+supprimer_val(A,"inser0")
+supprimer_val(B,"inser0")
+supprimer_val(C,"inser0")
+supprimer_val(A,"ajouter1")
+supprimer_val(B,"ajouter1")
+supprimer_val(C,"ajouter1")
+
+print(f"Liste :\n{A}\n{B}\n{C}")
+
+
+
+"""
+
+def supprimer_ind(chain,i) :
+	if len(chain) == 0 :
+		print("vide")
+
+	seg = chain
+	for i in range(i) :
+		seg = seg[1]
+
 	seg[0:] = seg[1]
 	return chain
 
-print("test supp",supprimer_val(A,14))
+
+def modifier(chain,i,element) :
+	if len(chain) == 0:
+		print("vide")
+
+	seg = chain
+	for i in range(i) :
+		seg=seg[1]
+
+	seg[0:] = [element,seg[1]]
+	return chain
+
+
+def vider(A) :
+	A = [None,None]
+	return A
+
 
 
 from random import*
@@ -102,5 +159,4 @@ cara = ["a","z","e","r","t","y","u","i","o","p","q","s","d","f","g",
 def rec() :
 	global cara
 	print( 	cara[randint(0,len(cara))]   )
-	rec()
-
+	rec()"""
